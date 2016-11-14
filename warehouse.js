0f2f2294cbb4c75a89d1c358b9f7a9ecc497e127
Fatima Saghir <fatima.saghir@live.it>
2016-11-14 12:59:02 +0100

    added submitItem() with its supporting functions(controlName, controlQuantity, submitAlert), insertItem just declared

--- warehouse.js
+++ warehouse.js
@@ -46,3 +46,65 @@ function readTableItems(names,quantities)
 		quantities.push(parseInt(secondRow.cells[i].innerHTML));
 	}
 }
+
+/**
+ *@brief inserts the new Item inserted in the form, it will first check if the input is valid; if it is valid the function will insert the Data of the new Item, otherwise the user is alerted; in either case the form will be cleared
+ *@param in string id Id of the form containing the input, it's expected to contain itemName and itemQuantity as fields.
+ */
+function submitItem(formId)
+{
+    var formH = document.getElementById(formId);
+    //copies the name and quantity inserted as input in the vat name and quantity
+    var name = formH.ItemName.value;
+	var quantity = formH.ItemQuantity.value;
+    if(controlName(name) && controlQuantity(quantity)){
+        hideElement(formId);
+        insertItem(name,parseInt(quantity));
+    }
+    else{
+        submitAlert();
+    }
+    //clears the form
+    document.getElementById(formId).reset();
+    
+}
+
+/**
+ *@Checks if the name is valid
+ *@param name Name that has to be checked
+ *@return True if the input is valid, false otherwise.
+ */
+function controlName(name){
+    //checks if name is valid, that is: 
+	//  not( name is empty or lenght is 0 or made of white spaces only)
+    return !(!name || 0 === name.length || !name.trim());
+}
+
+/**
+ *@Checks if the quantity is valid
+ *@param quantity Quantity that has to be checked
+ *@return True if the input is valid, false otherwise.
+ */
+function controlQuantity(quantity){
+    var parsed= parseInt(quantity);
+    return ((quantity == parsed) && parsed > 0);
+}
+
+/**
+ * @brief Tells the user the input was in an incorrect format.
+ */
+function submitAlert()
+{
+	alert("The input falls outside the required format. You have to insert a proper string as name and an integer larger than 0 as quantity.");
+}
+
+/**
+ * @brief Adds name and quantity data to the arrays storing them.
+ * @param in string name Name of the item to add.
+ * @param in string quantity Quantity of the item to add.
+ */
+function insertItem(name,quantity)
+{
+	
+}
+
