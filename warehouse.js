70b4a9e3f22a5e600ad3a4781e2dab492a00b95d
Fatima Saghir <fatima.saghir@live.it>
2016-11-14 10:31:22 +0100

    added devlarations of variables

---
+++ warehouse.js
@@ -0,0 +1,13 @@
+//list of the items' names
+var itemNames = [];
+
+//list of the items' quantities
+var itemQuantities = [];
+// the i-th item has itemNames[i] name and itemQuanties[i] quantity
+
+//maximum storage value
+var maxStorageValue;
+
+//a variable to handle the information about the items: the first row contains the names and the second the quantities
+var tableHandle = document.getElementById("itemsNamesQuantities");
+
