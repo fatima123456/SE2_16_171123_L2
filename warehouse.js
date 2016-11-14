6337f7d63ad05b78946881f63f2f6d180ae4beab
Fatima Saghir <fatima.saghir@live.it>
2016-11-14 11:03:10 +0100

    added showElement() and hideElement()

--- warehouse.js
+++ warehouse.js
@@ -11,3 +11,20 @@ var maxStorageValue;
 //a variable to handle the information about the items: the first row contains the names and the second the quantities
 var tableHandle = document.getElementById("itemsNamesQuantities");
 
+/**
+ * @brief It sets the visibility property of the html element to visible.
+ * @param in string id Id of the element to show.
+ */
+function showElement(id)
+{
+	document.getElementById(id).style.visibility = "visible";
+}
+
+/**
+ * @brief It sets the visibility property of the html element to hidden.
+ * @param in string id Id of the element to show.
+ */
+function hideElement(id)
+{
+	document.getElementById(id).style.visibility = "hidden";
+}
